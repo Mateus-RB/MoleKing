aa18395d6974f3765c1fc97d46e4eec22ae059c9
phfmatias <pedrohfmatias@gmail.com>
2024-02-26 12:21:05 -0300

    Add RMSD calculation to Molecule class

--- src/main.cpp
+++ src/main.cpp
@@ -138,6 +138,7 @@ PYBIND11_MODULE(MoleKing, m) {
         .def("getIRCDihedrals", &Molecule::getIRCDihedrals)
         .def("removeElement", &Molecule::removeElement)
         .def("toXYZ", &Molecule::toXYZ, py::arg("fileName") = "MK_Molecule.xyz")
+        .def("RMSD", &Molecule::RMSD, py::arg("MOL2"))
         .def("toGJF", &Molecule::toGJF, py::arg("fileName") = "MK_Molecule.gjf", py::arg("method") = "B3LYP", py::arg("basis") = "6-311g(d)", py::arg("addKeywords") = "", py::arg("endKeywords") = "", py::arg("charge") = 0, py::arg("multiplicity") = 1, py::arg("zmatrix") = 0)
         .def("getMM", &Molecule::getMolecularMass);
     
